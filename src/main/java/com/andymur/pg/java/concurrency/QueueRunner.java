393c100af9bf4dad992652d210b1ac1b9bb4bbd4
andymur <andymurh@gmail.com>
2017-06-08 19:12:04 +0300

    blocking queue draft

---
+++ src/main/java/com/andymur/pg/java/concurrency/QueueRunner.java
@@ -0,0 +1,8 @@
+package com.andymur.pg.java.concurrency;
+
+/**
+ * Created by muraand.
+ */
+public class QueueRunner {
+
+}
---
+++ src/test/java/com/andymur/pg/java/concurrency/QueueTest.java
@@ -0,0 +1,47 @@
+package com.andymur.pg.java.concurrency;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+/**
+ * Created by muraand.
+ */
+public class QueueTest {
+
+    @Test(expected = IllegalStateException.class)
+    public void addTakeTest() {
+        Queue<Integer> queue = new Queue<Integer>(5);
+        queue.print();
+        queue.add(200);
+        queue.print();
+        queue.add(300);
+        queue.print();
+        Assert.assertEquals(new Integer(200), queue.take());
+        queue.print();
+        Assert.assertEquals(new Integer(300), queue.take());
+        queue.print();
+        queue.take();
+    }
+
+    @Test(expected = IllegalStateException.class)
+    public void addTakeOverlap() {
+        Queue<Integer> queue = new Queue<Integer>(3);
+        queue.print();
+        queue.add(200);
+        queue.print();
+        queue.add(300);
+        queue.print();
+        queue.add(400);
+        queue.print();
+        Assert.assertEquals(new Integer(200), queue.take());
+        queue.print();
+        queue.add(500);
+        queue.print();
+        queue.add(600);
+    }
+
+    @Test
+    public void addTakeTwiceOverlap() {
+
+    }
+}
