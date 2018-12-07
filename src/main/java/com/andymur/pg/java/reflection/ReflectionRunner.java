5b422d1b16e56f876dc0e4116cdff7d0f412cbde
andymur <andymurh@gmail.com>
2018-12-07 14:14:08 +0100

    reflection example

--- src/main/java/com/andymur/pg/java/reflection/ReflectionRunner.java
+++ src/main/java/com/andymur/pg/java/reflection/ReflectionRunner.java
@@ -1,4 +1,36 @@
 package com.andymur.pg.java.reflection;
 
+import java.lang.reflect.Field;
+import java.math.BigDecimal;
+import java.math.BigInteger;
+
 public class ReflectionRunner {
+
+	public static void main(String[] args) throws NoSuchFieldException, IllegalAccessException {
+		City saintP = new City("SaintP", BigDecimal.ONE);
+
+		Field rating = saintP.getClass().getDeclaredField("rating");
+		rating.setAccessible(true);
+		rating.set(saintP, new BigInteger("10"));
+
+		System.out.println(saintP);
+	}
+
+	public static class City {
+		private String name;
+		private BigDecimal rating;
+
+		public City(final String name, final BigDecimal rating) {
+			this.name = name;
+			this.rating = rating;
+		}
+
+		@Override
+		public String toString() {
+			return "City{" +
+					"name='" + name + '\'' +
+					", rating=" + rating +
+					'}';
+		}
+	}
 }
