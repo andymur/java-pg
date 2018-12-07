3934c2a4d4091905963284bd9c506493b5031e0a
andymur <andymurh@gmail.com>
2018-12-07 14:12:24 +0100

    add chess board example

--- src/main/java/com/andymur/pg/java/chessboard/ChessBoardRunner.java
+++ src/main/java/com/andymur/pg/java/chessboard/ChessBoardRunner.java
@@ -1,4 +1,31 @@
 package com.andymur.pg.java.chessboard;
 
 public class ChessBoardRunner {
+
+	static final int COL_NUM = 8;
+	static final int ROW_NUM = 8;
+
+	public static final String BLACK_CELL = " X";
+	public static final String WHITE_CELL = " O";
+
+	public static void main(String[] args) {
+		printBoard();
+	}
+
+	static void printBoard() {
+		for (int i = 0; i < COL_NUM; i++) {
+			String cell = i % 2 == 0 ? BLACK_CELL : WHITE_CELL;
+
+			for (int j = 0; j < ROW_NUM; j++) {
+				System.out.print(cell);
+				cell = nextCell(cell);
+			}
+			System.out.print("\n");
+		}
+	}
+
+	static String nextCell(String currentCell) {
+		return currentCell.equals(WHITE_CELL) ? BLACK_CELL : WHITE_CELL;
+	}
+
 }
