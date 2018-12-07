3ab230e7c2228ed929c1ad3b30886b4ca9eda505
andymur <andymurh@gmail.com>
2018-12-07 14:03:19 +0100

    collections

--- src/main/java/com/andymur/pg/java/collections/CollectionsRunner.java
+++ src/main/java/com/andymur/pg/java/collections/CollectionsRunner.java
@@ -1,4 +1,74 @@
 package com.andymur.pg.java.collections;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Comparator;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.function.Function;
+
 public class CollectionsRunner {
+
+	public static void main(String[] args) {
+		/*List<String> p = Arrays.asList("Venus", "Terra", "Pluto");
+		List<String> np = new ArrayList<>(p.subList(0, 1)); np.add("Mars");
+		System.out.println(np);*/
+		Map<String, Integer> cityToPopulation = new HashMap<>();
+
+		cityToPopulation.put("Paris", 2_000_000);
+		cityToPopulation.put("Moscow", 12_000_000);
+
+		Function<String, Integer> unknownCityPopulation = cityName -> 0;
+
+		cityToPopulation.computeIfAbsent("Saint Petersburg", unknownCityPopulation);
+		cityToPopulation.computeIfAbsent("Moscow", unknownCityPopulation);
+		cityToPopulation.computeIfAbsent("Tehran", unknownCityPopulation);
+
+		System.out.println(cityToPopulation);
+
+		List<City> cities = new ArrayList<>(cityToPopulation.size());
+
+		cityToPopulation.forEach((cityName, population) ->
+			cities.add(City.of(cityName, population))
+		);
+
+		System.out.println(cities);
+		cities.sort(Comparator.comparingInt(City::getPopulation).thenComparing(City::getName).reversed());
+		System.out.println(cities);
+	}
+
+	static class City {
+		private final String name;
+		private final int population;
+
+		public static City of(final String name, final int population) {
+			return new City(name, population);
+		}
+
+		public static City  of(final String name) {
+			return of(name, 0);
+		}
+
+		private City(final String name, final int population) {
+			this.name = name;
+			this.population = population;
+		}
+
+		@Override
+		public String toString() {
+			return "City{" +
+					"name='" + name + '\'' +
+					", population=" + population +
+					'}';
+		}
+
+		public String getName() {
+			return name;
+		}
+
+		public int getPopulation() {
+			return population;
+		}
+	}
 }
--- src/main/java/com/andymur/pg/java/collections/StreamCollectionRunner.java
+++ src/main/java/com/andymur/pg/java/collections/StreamCollectionRunner.java
@@ -1,4 +1,31 @@
 package com.andymur.pg.java.collections;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.function.Function;
+import java.util.function.Supplier;
+
 public class StreamCollectionRunner {
+
+	public static void main(String[] args) {
+		Supplier<Boolean> supTrue = () -> true;
+
+		List<Boolean> allTrue = new ArrayList<>(Arrays.asList(true, true, true));
+		List<Boolean> someTrue = new ArrayList<>(Arrays.asList(true, true, false));
+		List<Boolean> allFalse = new ArrayList<>(Arrays.asList(false, false, false));
+
+
+		System.out.println("ALL MATCH ALL TRUE: " + allTrue.stream().allMatch(aBoolean -> aBoolean));
+		System.out.println("ALL MATCH SOME TRUE: " + someTrue.stream().allMatch(aBoolean -> aBoolean));
+		System.out.println("ALL MATCH ALL FALSE: " + allFalse.stream().allMatch(aBoolean -> aBoolean));
+
+		System.out.println("=====================");
+
+		System.out.println("ANY MATCH ALL TRUE: " + allTrue.stream().anyMatch(aBoolean -> aBoolean));
+		System.out.println("ANY MATCH SOME TRUE: " + someTrue.stream().anyMatch(aBoolean -> aBoolean));
+		System.out.println("ANY MATCH ALL FALSE: " + allFalse.stream().anyMatch(aBoolean -> aBoolean));
+
+		System.out.println(Boolean.parseBoolean("true"));
+	}
 }
