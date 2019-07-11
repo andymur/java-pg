b1f7ef63380b97af781d47dfad99610ba10e9ea7
andymur <andymurh@gmail.com>
2019-07-11 13:49:07 +0200

    Add log props and Camel http download example

---
+++ java-playground-camel/src/main/java/com/andymur/pg/java/CamelRunner.java
@@ -0,0 +1,49 @@
+package com.andymur.pg.java;
+
+import java.util.concurrent.TimeUnit;
+
+import org.apache.camel.CamelContext;
+import org.apache.camel.Exchange;
+import org.apache.camel.LoggingLevel;
+import org.apache.camel.builder.RouteBuilder;
+import org.apache.camel.component.http.HttpComponent;
+import org.apache.camel.component.jetty.JettyHttpComponent;
+import org.apache.camel.component.jetty9.JettyHttpComponent9;
+import org.apache.camel.impl.DefaultCamelContext;
+
+public class CamelRunner {
+	public static void main(String[] args) throws Exception {
+		CamelContext camelContext = new DefaultCamelContext();
+		camelContext.setTracing(false);
+
+		JettyHttpComponent jettyComponent = new JettyHttpComponent9();
+		HttpComponent httpComponent = new HttpComponent();
+
+		org.apache.camel.component.http4.HttpComponent httpComponent4 = new org.apache.camel.component.http4.HttpComponent();
+
+		camelContext.addComponent("jetty", jettyComponent);
+		camelContext.addComponent("http", httpComponent4);
+
+		final RouteBuilder routeBuilder = new RouteBuilder() {
+
+			@Override
+			public void configure() throws Exception {
+				//from("direct:start").to("jetty:http://www.lib.ru/KIPLING/kim.txt");
+				from("direct:start")
+						.log(LoggingLevel.INFO,"hey just accessed something on the web")
+						.setHeader(Exchange.HTTP_METHOD, constant("GET"))
+						//.to("jetty:http://www.lib.ru/KIPLING/kim.txt")
+						.toD("http://www.lib.ru/KIPLING/kim.txt")
+						//.process(new LogProcessor())
+						.log(LoggingLevel.INFO,"hey just accessed something on the web")
+						.to("file:/tmp/kim.txt");
+			}
+		};
+
+		camelContext.addRoutes(routeBuilder);
+
+		camelContext.start();
+		Thread.sleep(TimeUnit.SECONDS.toMillis(5));
+		camelContext.stop();
+	}
+}
